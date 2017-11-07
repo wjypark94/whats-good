61edc2cff10f5a5f7b355c54b3092d388748777c
Mike <Mdubbpro@gmail.com>
2017-11-07 15:27:06 -0800

    Added basic Mongoose database with get and write methods

---
+++ server/db/mongo/controller.js
@@ -0,0 +1,90 @@
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/',{useMongoClinet:true})
+mongoose.Promise = global.Promise
+
+db.once('open'),()=>{
+  console.log('Connected to the DB')
+}
+
+var ItinerarySchema = new mongoose.Schema({
+  listname: String,
+  listid: Number,
+  userid: String,
+  date: String, // This might need to be DATE, but it's not a great format
+  eventids: [],
+  eventtime: [],
+})
+
+var EventSchema = new mongoose.Schema({
+  eventname: String,
+  eventid: Number,
+  description: String,
+  image: String,
+  location: String,
+})
+
+var Itinerary = mongoose.model('Itinerary', ItinerarySchema)
+var Event = mongoose.model('Event', EventSchema)
+
+// Helper functions:
+
+var getItem = function(type){
+  db.type.find({}, (err, data) =>{
+    if (err) {
+      return console.log (err)
+    } else {
+      return data;
+      console.log(data) //Slash this out later
+    }
+  })
+}
+
+// Will need options for an ID or something specific
+// Possibly a function that reads an Itinerary, 
+// Returns all event IDS in the event ID array,
+
+var saveEvent = function(passE){
+
+  var newE = new db.Event;
+  newE.eventname = passE.eventname;
+  newE.eventid = passE.eventid;
+  newE.description = passE.description;
+  newE.image = passE.image;
+  newE.location = passE.location;
+
+  newE.save(function (err, newE){
+    if (err) return console.log(err);
+    console.log('Event successfully added to db!')
+  })
+  return(newE);
+}
+
+var saveItinerary = function(passI){
+
+  var newI = new db.Itinerary;
+  newI.listname = passI.listname;
+  newI.listid = passI.listid;
+  newI.userid = passI.userid;
+  newI.date = passI.date;
+  newI.eventids = passI.eventids;
+  newI.eventtime = passI.eventtime;
+
+  newI.save(function (err, newItinerary){
+    if (err) return console.log(err);
+    console.log('Itinerary successfully added to db!')
+  })
+  return(newI);
+}
+
+
+// var ItinerarySchema = new mongoose.Schema({
+//   listname: String,
+//   listid: Number,
+//   userid: String,
+//   date: String, // This might need to be DATE, but it's not a great format
+//   eventids: [],
+//   eventtime: [],
+// })
+
+
+module.exports = {Itinerary, Event}
