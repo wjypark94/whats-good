df1a4eb7327dd99d77a0747d0a5d02f0d27ed12c
Zac St Louis <p2kmafia@gmail.com>
2017-11-08 15:37:42 -0800

    fixed conflict itenerary.js

--- client/components/itinerary.js
+++ client/components/itinerary.js
@@ -5,7 +5,7 @@ angular.module('whatsGood')
     controller: function() {
       //remove after bindings so not overwritten
       this.itinerary = {};
-      this.itinerary.imagePath = 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/santa-monica-pier-kinga-szymczyk.jpg';
+      this.itinerary.imagePath = 'http://dandelionmoms.com/wp-content/uploads/2012/11/Photo.SantaMonica.4541889759_89e6bdc232_z.jpg';
       this.itinerary.itineraryName = 'SM Trip with the boys';
     },
     template: `
@@ -15,8 +15,8 @@ angular.module('whatsGood')
           <section flex="80">
             <md-content class="md-padding" layout-xs="column" layout="row">
               <div flex-xs flex-gt-xs="95" layout="column">
-                <md-card style="min-height:75%;">
-                  <div style="position:relative;">
+                <md-card>
+                  <div style="position:relative">
                     <img flex="100" ng-src="{{$ctrl.itinerary.imagePath}}" class="md-card-image" alt="Washed Out">
                     <div style="position:absolute; bottom:0px; left:0px; height:auto; width:100%; text-align:center; font-size:1.5em; padding: 10px 0px; background-color:rgba(0,0,0,0.6)">
                       {{$ctrl.itinerary.itineraryName}}
@@ -24,22 +24,18 @@ angular.module('whatsGood')
                   </div>
                   <md-card-content>
                     <p>
-                      List Item 1
+                      The titles of Washed Out's breakthrough song and the first single from Paracosm share the
+                      two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well...
                     </p>
                     <p>
-                      List Item 2
+                      The titles of Washed Out's breakthrough song and the first single from Paracosm share the
+                      two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well...
                     </p>
                     <p>
-                      List Item 3
-                    </p>
-                    <p>
-                      List Item 4
-                    </p>
-                    <p>
-                      List Item 5
+                      The titles of Washed Out's breakthrough song and the first single from Paracosm share the
+                      two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well...
                     </p>
                   </md-card-content>
-                  <span flex></span>
                   <md-card-actions layout="row" layout-align="end center">
                     <md-button>Action 1</md-button>
                     <md-button>Action 2</md-button>
